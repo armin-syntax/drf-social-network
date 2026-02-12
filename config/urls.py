4470cc557e1bc306036b8bf09f01dcf5514fd378
Armin <armin.lambda@gmail.com>
2026-02-12 18:41:30 +0330

    edit config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
